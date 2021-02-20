ed3628488cd77faad55e314948ad45a1f272c7c5
Robyn <robynthecryptid66@gmail.com>
2021-02-20 18:38:14 +1100

    0.0.0.4
    - Added paragraph function, which allows for the creation of paragraphs.
    - changed all non docstring instances of document to doc
    - Added docstrings to:  heading, paragraph.

--- htmlinpython.py
+++ htmlinpython.py
@@ -1,22 +1,29 @@
-def initDoc(documentName):
-    with open(f"{documentName}.html", 'x') as document:
-        document.write("<!DOCTYPE html>\n")
-        document.write("<html>\n")
-        document.write(f"<title>{documentName}</title>\n")
-        document.write(f"<body>\n")
-        document.close()
+def initDoc(docName):
+    with open(f"{docName}.html", 'x') as doc:
+        doc.write("<!DOCTYPE html>\n")
+        doc.write("<html>\n")
+        doc.write(f"<title>{docName}</title>\n")
+        doc.write(f"<body>\n")
+        doc.close()
     """Creates and initialises the HTML document. ONLY USE ONCE PER DOCUMENT"""
 
 
-def endDoc(documentName):
-    with open(f"{documentName}.html", 'a') as document:
-        document.write("</body>\n")
-        document.write("</html>\n")
-        document.close()
+def endDoc(docName):
+    with open(f"{docName}.html", 'a') as doc:
+        doc.write("</body>\n")
+        doc.write("</html>\n")
+        doc.close()
     """Adds the closing tags to an HTML Document. ONLY USE AT THE END OF A DOCUMENT"""
 
 
 def heading(docName, headSize, contents):
-    with open(f"{docName}.html", 'a') as document:
-        document.write(f"<h{str(headSize)}>{contents}</h{str(headSize)}>")
-        document.close()
+    with open(f"{docName}.html", 'a') as doc:
+        doc.write(f"<h{str(headSize)}>{contents}</h{str(headSize)}>")
+        doc.close()
+    """Adds a heading to an HTML Document, in a size of your choice."""
+
+
+def paragraph(docName, contents):
+    with open(f"{docName}.html", 'a') as doc:
+        doc.write(f"<p>{contents}</p>")
+    """Adds a paragraph to an HTML document. </b> must be manually added into contents."""
